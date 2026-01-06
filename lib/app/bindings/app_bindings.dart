a022fd1494f351a65bb69e59d6fb68787140c160
Muhammad Younis <binnazar7575@gmail.com>
2026-01-06 19:13:18 +0500

    final  comment

--- lib/app/bindings/app_bindings.dart
+++ lib/app/bindings/app_bindings.dart
@@ -8,24 +8,19 @@ import 'package:mini_social/presentation/controllers/post_controller.dart';
 
 class AppBindings extends Bindings {
   @override
-  void dependencies() {
-    // 1. Services
+  void dependencies() { 
     Get.lazyPut(() => FirebaseAuthService(), fenix: true);
     Get.lazyPut(() => FirestoreService(), fenix: true);
-    
-    // 2. Repositories (Must be before Controllers)
+     
     Get.lazyPut(() => AuthRepository(
       authService: Get.find<FirebaseAuthService>(),
       firestoreService: Get.find<FirestoreService>(),
     ), fenix: true);
-
-    // ADD THIS LINE - It was missing!
+ 
     Get.lazyPut(() => PostRepository(
       firestoreService: Get.find<FirestoreService>(),
     ), fenix: true);
-
-    // 3. Controllers
-    // permanent: true ensures AuthController isn't deleted on navigation 
+ 
     Get.put(
       AuthController(authRepository: Get.find<AuthRepository>()),
       permanent: true,
